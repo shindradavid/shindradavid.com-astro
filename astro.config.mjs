50e9177427c05eb8315aac27e8fc0d6e77d0e7fe
shindra david <shindradavid.dev@gmail.com>
2022-10-14 22:59:57 +0300

    Add sitemap package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+	site: 'https://shindradavid.vercel.app',
 	markdown: {
 		shikiConfig: {
 			theme: 'dark-plus',
 			// Enable word wrap to prevent horizontal scrolling
 			wrap: false
 		}
-	}
+	},
+	integrations: [sitemap()]
 });
