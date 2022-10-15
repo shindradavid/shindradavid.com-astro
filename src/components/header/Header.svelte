cee9a835788ba6bb2652233330fd6bf77231b2ba
shindra david <shindradavid.dev@gmail.com>
2022-10-15 13:12:38 +0300

    Add MainLayout to all pages

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts"></script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/portfolio">Portfolio</a>
+		<a href="/contact">Contact</a>
+		<a href="/FAQ">FAQ</a>
+		<div class="divider" />
+		<a href="/blog">Blog</a>
+		<a href="/courses">Courses</a>
+	</nav>
+</header>
+
+<style lang="scss"></style>
